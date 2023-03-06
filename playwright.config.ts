7e21267c043b27a86d1dd1c0ef4bd6e455260ec2
josh-keegan <josh.keegan@hyland.com>
2023-03-06 17:57:36 -0500

    Upload tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
 
   reporter: [
-    ["html", { outputFolder: "Test-Results" + new Date().toISOString() }],
+    ["html", { outputFolder: "playwright-report" + new Date().toISOString() }],
   ],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
